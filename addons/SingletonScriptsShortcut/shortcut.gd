116d87da93bf0055977a133e65fc789b478a5f38
Salvakiya <6888784+Salvakiya@users.noreply.github.com>
2021-09-05 15:44:00 -0700

    Singletons sorted alphabetically
    Sort the singletons alphabetically

--- addons/SingletonScriptsShortcut/shortcut.gd
+++ addons/SingletonScriptsShortcut/shortcut.gd
@@ -4,8 +4,13 @@ onready var scriptIconTexture = get_icon("Script", "EditorIcons")
 var popup = get_popup()
 
 func add_singletons(dictionary, plugin, currentlyOpen):
-	var dictionaryKeys = dictionary.keys()
-	var dictionaryValues = dictionary.values()
+	var dictionaryKeys:Array = dictionary.keys()
+	var dictionaryValues:Array# = dictionary.values()
+	dictionaryKeys.sort()
+
+	for i in dictionaryKeys.size():
+		dictionaryValues.append(dictionary[dictionaryKeys[i]])
+	
 	for i in dictionary.size():
 		popup.add_item(dictionaryKeys[i])
 		popup.set_item_metadata(i, dictionaryValues[i])
